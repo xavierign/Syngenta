92d6e90d38eab3d4fb69ed57ef27402d26463838
XAVIER IGNACIO GONZALEZ <XAVIERIGN@GMAIL.COM>
2016-01-02 18:27:36 -0500

    Changes

--- Robust Decisions Syngenta Challenge.R
+++ Robust Decisions Syngenta Challenge.R
@@ -1,3 +1,4 @@
+## github
 #more information about the contest https://www.ideaconnection.com/syngenta-crop-challenge/
 
 ##---- 0. Libraries ----
@@ -53,7 +54,23 @@ trainevalDK <- read.xlsx(infile5,sheet=1,
                      colNames=T,
                      startRow = 1)
 
-##---- 2. ANALYZE THE DATA----
+##---- 2. VISUALIZE THE DATA----
+
+lat_m <- sum(range(trainDS$LAT))/2
+lon_m <- sum(range(trainDS$LONG_))/2
+
+myLocation <- c(lon = lon_m, lat = lat_m)
+
+myMap <- get_map(location=myLocation,
+                 source="google", maptype="terrain", crop=FALSE, zoom=5)
+
+ggmap(myMap) + 
+  geom_point(aes(x = LONG_, y = LAT), data = trainDS[trainDS$VARIETY=='V39',],
+             alpha = .3, color="blue", size = 1) +
+  geom_point(aes(x = LONG_, y = LAT), data = evalDS,
+             alpha = .6, color="red", size = 1)
+
+##---- 3. CONVERT AND ANALYZE THE DATA----
 
 #convert SITE to factor
 trainDS$SITE <- as.factor(trainDS$SITE)
@@ -77,6 +94,11 @@ trainDS$CLIMATE3<- as.numeric(substring(trainDS$CLIMATE,3,3))
 #CLIMATE 2 AND CLIMATE 3 HAVE MISSING VALES
 trainDS$CLIMATE2[is.na(trainDS$CLIMATE2)] <- 0
 trainDS$CLIMATE3[is.na(trainDS$CLIMATE3)] <- 0
+
+#decompose SOIL_CUBE in 3
+trainDS$SOIL_CUBE1<- as.numeric(substring(trainDS$SOIL_CUBE,1,1))
+trainDS$SOIL_CUBE2<- as.numeric(substring(trainDS$SOIL_CUBE,2,2))
+trainDS$SOIL_CUBE3<- as.numeric(substring(trainDS$SOIL_CUBE,3,3))
 
 #convert RM_BAND into a number
 RM_BAND_N <- rep(1,length(trainDS$RM_BAND))
@@ -103,42 +125,25 @@ ssv <- ssv[!duplicated(ssv),]
 ssv.pivot = dcast(ssv, SITE ~ SEASON, value.var = "VARIETY")
 ssv.pivot
 
-#the data is very sparesed! can not define combinations at this point.
-
-##---- 3. VISUALIZE THE DATA----
-
-lat_m <- sum(range(trainDS$LAT))/2
-lon_m <- sum(range(trainDS$LONG_))/2
-
-myLocation <- c(lon = lon_m, lat = lat_m)
-
-myMap <- get_map(location=myLocation,
-                 source="google", maptype="terrain", crop=FALSE, zoom=4)
-
-ggmap(myMap) + 
-  geom_point(aes(x = LONG_, y = LAT), data = trainDS,
-             alpha = .1, color="blue", size = 1) +
-  geom_point(aes(x = LONG_, y = LAT), data = evalDS,
-             alpha = .6, color="red", size = 1)
-
-   
-
-##---- 4. AGREGATION OF WEATHER DATA----
+##---- 5. AGREGATION OF WEATHER DATA----
 #define the temrs 1, 2 and 3
 
-start0 <- 91
-start1 <- 112
-start2 <- 173
-start3 <- 235
-end3 <- 305
+start0 <- 61
+start1 <- 119
+start2 <- 149
+end3 <- 275
+
+start3 <- 209
 
 #define the booleans t1 t2 t3
 
 t0 <- trainDDS$yday>=start0&trainDDS$yday<start1
 t1 <- trainDDS$yday>=start1&trainDDS$yday<start2
-t2 <- trainDDS$yday>=start2&trainDDS$yday<start3
+t2 <- trainDDS$yday>=start2&trainDDS$yday<end3
+
+#these are not used:
 t3 <- trainDDS$yday>=start3&trainDDS$yday<=end3
-ta <- trainDDS$yday>=start0&trainDDS$yday<=end3
+ta <- trainDDS$yday>=1&trainDDS$yday<=365
 
 #column of years
 agg.data <- unique(trainDDS[,c('year','Site','Scenario')])
@@ -249,14 +254,15 @@ agg.data <- cbind(agg.data,swe.t0.me)
 swe.t1.me <- tapply (trainDDS$swe[t1],INDEX=trainDDS$Scenario[t1],FUN=mean)
 agg.data <- cbind(agg.data,swe.t1.me)
 
-swe.t2.me <- tapply (trainDDS$swe[t2],INDEX=trainDDS$Scenario[t2],FUN=mean)
-agg.data <- cbind(agg.data,swe.t2.me)
+#no snow in t2 and t3
+#swe.t2.me <- tapply (trainDDS$swe[t2],INDEX=trainDDS$Scenario[t2],FUN=mean)
+#agg.data <- cbind(agg.data,swe.t2.me)
 
-swe.t3.me <- tapply (trainDDS$swe[t3],INDEX=trainDDS$Scenario[t3],FUN=mean)
-agg.data <- cbind(agg.data,swe.t3.me)
+#swe.t3.me <- tapply (trainDDS$swe[t3],INDEX=trainDDS$Scenario[t3],FUN=mean)
+#agg.data <- cbind(agg.data,swe.t3.me)
 
-swe.ta.me <- tapply (trainDDS$swe[ta],INDEX=trainDDS$Scenario[ta],FUN=mean)
-agg.data <- cbind(agg.data,swe.ta.me)
+#swe.ta.me <- tapply (trainDDS$swe[ta],INDEX=trainDDS$Scenario[ta],FUN=mean)
+#agg.data <- cbind(agg.data,swe.ta.me)
 
 #tmax-ave
 tmax.t0.me <- tapply (trainDDS$tmax[t0],INDEX=trainDDS$Scenario[t0],FUN=mean)
@@ -435,8 +441,9 @@ agg.data <- cbind(agg.data,vp.t2.ma)
 vp.t3.ma <- tapply (trainDDS$vp[t3],INDEX=trainDDS$Scenario[t3],FUN=max)
 agg.data <- cbind(agg.data,vp.t3.ma)
 
-vp.ta.ma <- tapply (trainDDS$vp[ta],INDEX=trainDDS$Scenario[ta],FUN=max)
-agg.data <- cbind(agg.data,vp.ta.ma)
+#colinieality
+#vp.ta.ma <- tapply (trainDDS$vp[ta],INDEX=trainDDS$Scenario[ta],FUN=max)
+#agg.data <- cbind(agg.data,vp.ta.ma)
 
 #vp-min
 vp.t0.mi <- tapply (trainDDS$vp[t0],INDEX=trainDDS$Scenario[t0],FUN=min)
@@ -457,41 +464,131 @@ agg.data <- cbind(agg.data,vp.ta.mi)
 #left join.
 trainDS <- merge(trainDS, agg.data, by = c('SITE','SEASON'))
 
-##---- 4. PCA-----
-#create PCA with complete cases
-formula0 <- '~RM_BAND_N + AREA + CLIMATE1 + CLIMATE2 + CLIMATE3 + RM_25 + TOT_IRR_DE + 
- SY_DENS + SY_ACRES + CONUS_PH + CONUS_AWC + CONUS_CLAY + CONUS_SILT + 
-CONUS_SAND +
-ISRIC_SAND + ISRIC_SILT + ISRIC_CLAY + ISRIC_PH + ISRIC_CEC + EXTRACT_CE +
-dwr.t0.me+  dwr.t1.me+  dwr.t2.me+  dwr.t3.me+  dwr.t0.ma+  dwr.t1.ma+
-dwr.t2.ma+  dwr.t3.ma+ 
-dayl.t0.me +
-dayl.t1.me+dayl.t2.me+dayl.t3.me+prcp.t0.me+prcp.t1.me+prcp.t2.me +
-prcp.t3.me+srad.t0.me+srad.t1.me+srad.t2.me+srad.t3.me+
-swe.t0.me+ swe.t1.me+ swe.t2.me+ swe.t3.me+ tmax.t0.me+tmax.t1.me +
-tmax.t2.me+tmax.t3.me+tmax.t0.ma+tmax.t1.ma+tmax.t2.ma+tmax.t3.ma +
-tmax.t0.mi+tmax.t1.mi+tmax.t2.mi+tmax.t3.mi+tmin.t0.me +
-tmin.t1.me+tmin.t2.me+tmin.t3.me+tmin.t0.ma+tmin.t1.ma+tmin.t2.ma +
-tmin.t3.ma+tmin.t0.mi+tmin.t1.mi+tmin.t2.mi+tmin.t3.mi+
-trange.t0.me+
-trange.t1.me+trange.t2.me+trange.t3.me+trange.t0.ma+trange.t1.ma+
-trange.t2.ma +
-trange.t3.ma+trange.t0.mi+trange.t1.mi+trange.t2.mi+trange.t3.mi+
+#consider varialbes TEMPXX, PRECXX, RADXX
+trainDS$TEMP_CUR <- rep(NA,length(trainDS$SITE))
+trainDS$PREC_CUR <- rep(NA,length(trainDS$SITE))
+trainDS$RAD_CUR <- rep(NA,length(trainDS$SITE))
 
-vp.t0.me+  vp.t1.me+  vp.t2.me+  vp.t3.me+    vp.t0.ma+  vp.t1.ma+
-vp.t2.ma+  vp.t3.ma+  vp.t0.mi+  vp.t1.mi+  vp.t2.mi+  vp.t3.mi' 
+#08
+trainDS$TEMP_CUR[trainDS$SEASON==2008]<- trainDS$TEMP_08[trainDS$SEASON==2008]
+trainDS$PREC_CUR[trainDS$SEASON==2008]<- trainDS$PREC_08[trainDS$SEASON==2008]
+trainDS$RAD_CUR[trainDS$SEASON==2008]<- trainDS$RAD_08[trainDS$SEASON==2008]
+#09
+trainDS$TEMP_CUR[trainDS$SEASON==2009]<- trainDS$TEMP_09[trainDS$SEASON==2009]
+trainDS$PREC_CUR[trainDS$SEASON==2009]<- trainDS$PREC_09[trainDS$SEASON==2009]
+trainDS$RAD_CUR[trainDS$SEASON==2009]<- trainDS$RAD_09[trainDS$SEASON==2009]
+#10
+trainDS$TEMP_CUR[trainDS$SEASON==2010]<- trainDS$TEMP_10[trainDS$SEASON==2010]
+trainDS$PREC_CUR[trainDS$SEASON==2010]<- trainDS$PREC_10[trainDS$SEASON==2010]
+trainDS$RAD_CUR[trainDS$SEASON==2010]<- trainDS$RAD_10[trainDS$SEASON==2010]
+#11
+trainDS$TEMP_CUR[trainDS$SEASON==2011]<- trainDS$TEMP_11[trainDS$SEASON==2011]
+trainDS$PREC_CUR[trainDS$SEASON==2011]<- trainDS$PREC_11[trainDS$SEASON==2011]
+trainDS$RAD_CUR[trainDS$SEASON==2011]<- trainDS$RAD_11[trainDS$SEASON==2011]
+#12
+trainDS$TEMP_CUR[trainDS$SEASON==2012]<- trainDS$TEMP_12[trainDS$SEASON==2012]
+trainDS$PREC_CUR[trainDS$SEASON==2012]<- trainDS$PREC_12[trainDS$SEASON==2012]
+trainDS$RAD_CUR[trainDS$SEASON==2012]<- trainDS$RAD_12[trainDS$SEASON==2012]
+#13
+trainDS$TEMP_CUR[trainDS$SEASON==2013]<- trainDS$TEMP_13[trainDS$SEASON==2013]
+trainDS$PREC_CUR[trainDS$SEASON==2013]<- trainDS$PREC_13[trainDS$SEASON==2013]
+trainDS$RAD_CUR[trainDS$SEASON==2013]<- trainDS$RAD_13[trainDS$SEASON==2013]
+#14
+trainDS$TEMP_CUR[trainDS$SEASON==2014]<- trainDS$TEMP_14[trainDS$SEASON==2014]
+trainDS$PREC_CUR[trainDS$SEASON==2014]<- trainDS$PREC_14[trainDS$SEASON==2014]
+trainDS$RAD_CUR[trainDS$SEASON==2014]<- trainDS$RAD_14[trainDS$SEASON==2014]
+
+##---- 6. PCA-----
+#create PCA with complete cases
+#define predictors
+predictors.soil <- c("LAT","LONG_",
+"AREA","RM_25","TOT_IRR_DE",
+"SY_DENS","SY_ACRES","CONUS_PH","CONUS_AWC","CONUS_CLAY","CONUS_SILT",
+"CONUS_SAND","ISRIC_SAND","ISRIC_SILT","ISRIC_CLAY","ISRIC_PH","ISRIC_CEC",
+"EXTRACT_CE","SOIL_CUBE1","SOIL_CUBE2","SOIL_CUBE3","RM_BAND_N")
+
+predictors.weather <- c("CLIMATE1","CLIMATE2","CLIMATE3","TEMP_MED","PREC_MED","RAD_MED",
+"dayl.t0.me","dayl.t1.me","dayl.t2.me",
+"prcp.t0.me","prcp.t1.me","prcp.t2.me",
+"dwr.t0.me","dwr.t1.me","dwr.t2.me",
+"dwr.t0.ma","dwr.t1.ma","dwr.t2.ma",
+"srad.t0.me","srad.t1.me","srad.t2.me",
+"swe.t0.me","swe.t1.me",
+"tmax.t0.me","tmax.t1.me","tmax.t2.me",
+"tmax.t0.ma","tmax.t1.ma","tmax.t2.ma",
+"tmax.t0.mi","tmax.t1.mi","tmax.t2.mi",
+"tmin.t0.me","tmin.t1.me","tmin.t2.me",
+"tmin.t0.ma","tmin.t1.ma","tmin.t2.ma",
+"tmin.t0.mi","tmin.t1.mi","tmin.t2.mi",
+"trange.t0.me","trange.t1.me","trange.t2.me",
+"trange.t0.ma","trange.t1.ma","trange.t2.ma",
+"trange.t0.mi","trange.t1.mi","trange.t2.mi",
+"vp.t0.me","vp.t1.me","vp.t2.me",
+"vp.t0.ma","vp.t1.ma","vp.t2.ma",
+"vp.t0.mi","vp.t1.mi","vp.t2.mi",
+"TEMP_CUR","PREC_CUR","RAD_CUR")
+
+predictor.s.f <- paste('~',paste(predictors.soil, collapse = '+'),sep="")
+predictor.w.f <- paste('~',paste(predictors.weather, collapse = '+'),sep="")
 
 #define a scaled data frame
+PCAs <- prcomp(as.formula(predictor.s.f),
+               data=trainDS[,predictors.soil],
+              scale.=T)
 
-
-PCA <- princomp(as.formula(formula0), data=trainDS)
-#PCA <- princomp(dataSet[,c(1:6,26:(ncol(dataSet)-5))])
+PCAw <- prcomp(as.formula(predictor.w.f), 
+                 data=trainDS[,predictors.weather],
+               scale.=T)
 
 #append columns to dataSet
-trainDS <- cbind(trainDS,PCA$scores)
+comp.s <- PCAs$x
+comp.w <- PCAw$x
 
-PCAcalc <- loadings(PCA)
+colnames(comp.s) <- paste('COMP_S_',1:ncol(comp.s),sep='')
+colnames(comp.w) <- paste('COMP_W_',1:ncol(comp.w),sep='')
 
+trainDS <- cbind(trainDS,comp.s)
+trainDS <- cbind(trainDS,comp.w)
+PCAcalc.s <- PCAs$rotation
+PCAcalc.w <- PCAw$rotation
+
+##---- 4. EXPLORE VARIETIES INTO GENERATE MIX.----
+#parameters
+min.scenarios <- 20
+
+#create a table of varieties in each scenario.
+var.mat <- tapply(trainDS$VARIETY, list(trainDS$SEASON, trainDS$SITE), 
+                  FUN = function(x) unique(x))
+#count which are the pairs of varieties repeated in elements of a matrix.
+varieties <- names(table(trainDS$VARIETY)[table(trainDS$VARIETY)>min.scenarios])
+var.grid  <- NULL
+#generate combinations
+for (i in 1:(length(varieties)-1)){
+  for (j in (1+i):length(varieties)){
+    Var1 <- varieties[i]
+    Var2 <- varieties[j]
+    Count <- 0
+    var.grid <- rbind(var.grid,data.frame(Var1,Var2,Count))
+  }
+}
+
+#convert columns to string
+var.grid[, c(1,2)] <- sapply(var.grid[, c(1,2)], as.character)
+
+#loop 
+for (list in Filter(Negate(is.null), var.mat)){
+  for (row in 1:nrow(var.grid)){
+    if (sum(c(var.grid[row,1],var.grid[row,2] )%in% unlist(list))>1){
+      var.grid[row,3] <- var.grid[row,3] +1
+    }
+  }
+}
+
+var.grid[var.grid$Count>min.scenarios,]
+
+##---- . GENERATE MIX----
+
+for 
 
 ##---- 5. CANDIDATE SELECTION----
 #select the variety with the highest 1st quartile
@@ -504,8 +601,8 @@ first.q$count <- table(trainDS$VARIETY)
 first.q <- first.q[with(first.q, order(-VARIETY_YI)), ]
 
 #V39 seems to be the first candidate. V182 the second
-
 cand <- 'V39'
+
 #histogram of performances
 hist(trainDS$VARIETY_YI[trainDS$VARIETY==cand])
 
@@ -520,47 +617,26 @@ yield_t <- rep("GOOD", length(trainDS.c$VARIETY_YI))
 yield_t[trainDS.c$VARIETY_YI<target] <- "BAD" 
 trainDS.c$YIELD_T <- yield_t
 
-formula <- 'YIELD_T~ 
-    RM_BAND_N + AREA + CLIMATE1 + CLIMATE2 + CLIMATE3 + RM_25 + TOT_IRR_DE + SEASON1 +
-    SY_DENS + SY_ACRES + CONUS_PH + CONUS_AWC + CONUS_CLAY + CONUS_SILT + 
-    CONUS_SAND +
-    ISRIC_SAND + ISRIC_SILT + ISRIC_CLAY + ISRIC_PH + ISRIC_CEC + EXTRACT_CE +
-    dwr.t0.me+  dwr.t1.me+  dwr.t2.me+  dwr.t3.me+  dwr.ta.me+  dwr.t0.ma+  dwr.t1.ma+
-    dwr.t2.ma+  dwr.t3.ma+  dwr.ta.ma+ 
-    dayl.t0.me +
-    dayl.t1.me+dayl.t2.me+dayl.t3.me+dayl.ta.me+prcp.t0.me+prcp.t1.me+prcp.t2.me +
-    prcp.t3.me+prcp.ta.me+srad.t0.me+srad.t1.me+srad.t2.me+srad.t3.me+srad.ta.me +
-    swe.t0.me+ swe.t1.me+ swe.t2.me+ swe.t3.me+ swe.ta.me+ tmax.t0.me+tmax.t1.me +
-    tmax.t2.me+tmax.t3.me+tmax.ta.me+tmax.t0.ma+tmax.t1.ma+tmax.t2.ma+tmax.t3.ma +
-    tmax.ta.ma+tmax.t0.mi+tmax.t1.mi+tmax.t2.mi+tmax.t3.mi+tmax.ta.mi+tmin.t0.me +
-    tmin.t1.me+tmin.t2.me+tmin.t3.me+tmin.ta.me+tmin.t0.ma+tmin.t1.ma+tmin.t2.ma +
-    tmin.t3.ma+tmin.ta.ma+tmin.t0.mi+tmin.t1.mi+tmin.t2.mi+tmin.t3.mi+tmin.ta.mi +
-    trange.t0.me+
-    trange.t1.me+trange.t2.me+trange.t3.me+trange.ta.me+trange.t0.ma+trange.t1.ma+
-    trange.t2.ma +
-    trange.t3.ma+trange.ta.ma+trange.t0.mi+trange.t1.mi+trange.t2.mi+trange.t3.mi+
-    trange.ta.mi +
-    vp.t0.me+  vp.t1.me+  vp.t2.me+  vp.t3.me+  vp.ta.me+  vp.t0.ma+  vp.t1.ma+
-    vp.t2.ma+  vp.t3.ma+  vp.ta.ma+  vp.t0.mi+  vp.t1.mi+  vp.t2.mi+  vp.t3.mi+
-    vp.ta.mi +
-    Comp.1 + Comp.2 + Comp.3 + Comp.4 + Comp.5 + Comp.6 + Comp.7 + Comp.8 + Comp.9 + Comp.10+
-    Comp.11 + Comp.12 + Comp.13 + Comp.14 + Comp.15 + Comp.16 + Comp.17 + Comp.18 + Comp.19 + Comp.20+
-    Comp.21 + Comp.22 + Comp.23 + Comp.24 + Comp.25 + Comp.26 + Comp.27 + Comp.28 + Comp.29 + Comp.30+
-    Comp.31 + Comp.32 + Comp.33 + Comp.34 + Comp.35 + Comp.36 + Comp.37 + Comp.38 + Comp.39 + Comp.40+
-    Comp.41 + Comp.42 + Comp.43 + Comp.44 + Comp.45 + Comp.46 + Comp.47 + Comp.48 + Comp.49 + Comp.50+
-    Comp.51 + Comp.52 + Comp.53 + Comp.54 + Comp.55 + Comp.56 + Comp.57 + Comp.58 + Comp.59 + Comp.60+
-    Comp.61 + Comp.62 + Comp.63 + Comp.64 + Comp.65 + Comp.66 + Comp.67 + Comp.68 + Comp.69 + Comp.70+
-    Comp.71 + Comp.72 + Comp.73 + Comp.74 + Comp.75 + Comp.76 + Comp.77 + Comp.78 + Comp.79 + Comp.80+
-    Comp.81 + Comp.82 + Comp.83 + Comp.84 + Comp.85 + Comp.86 + Comp.87 + Comp.88 + Comp.89 + Comp.90+
-    Comp.91 + Comp.92' 
-  
+#define the tree formula
+#components
+formula.s <- paste(paste('COMP_S_',1:dim(PCAcalc.s)[1],sep=""),collapse='+')
+formula.w <- paste(paste('COMP_W_',1:dim(PCAcalc.w)[1],sep=""),collapse='+')
+comp <- paste(formula.s,formula.w,sep='+')
+
+#predictors
+predictors <- paste(paste(predictors.soil,collapse='+'),
+                    paste(predictors.weather,collapse='+'),
+                    sep='+')
+
+formula.t <- paste('YIELD_T',predictors,sep='~')
+formula.tree <- paste(formula.t,comp,sep='+')
+
+#parameters for the decision tree
 loss.matrix <- matrix(c(0, 1, 1, 0), nrow=2, byrow=TRUE)	
+uu8 <- rpart.control(minbucket = 4, maxdepth =30, xval=100)
 
-# --- Fit the classification tree.
-uu8 <- rpart.control(minbucket = 2, maxdepth =29, xval=100)
-
-fit.tree <- rpart(formula, data = trainDS.c, control=uu8,parms=list(loss = loss.matrix))
-#fit.tree <- rpart(formula, data = trainDS.c)
+#fit the tree!
+fit.tree <- rpart(formula.tree, data = trainDS.c, control=uu8,parms=list(loss = loss.matrix))
 
 #plot rpart
 y <- fit.tree$frame$yval2[,5]
@@ -584,16 +660,17 @@ bad.axis <- tapply(trainDS$VARIETY_YI[pred.tree == "BAD"],
 sort(bad.axis[is.na(good.axis)], decreasing=T)
 
 plot(bad.axis,good.axis,
-     xlim=c(30,80),
+     xlim=c(30,90),
      ylim=c(45,80),
      type="n")
 text (bad.axis,good.axis, names(good.axis),cex=0.7)
 text (bad.axis[cand],good.axis[cand], names(good.axis[cand]),cex=0.7, col='red')
 
 #review the fronteir candidates
-first.q[first.q$VARIETY %in% c('V59','V24','V15','V25', 'V87','V27','V24','V79','V200'),]
+first.q[first.q$VARIETY %in% c('V24','V22','V68', 'V81','V25','V200','V171'),]
 
 ##---- TODO NEXT----
 # 1. analize prim algorithm and scenario discovery.
-# 2. principal components are not selected-- try scale the matrix and outliers.
-# 3. clean the code and the formulas.
+# 2. outliers.
+# 4. generate combinations of variety
+# 5. fin the optimal t0, t1, t2, 
